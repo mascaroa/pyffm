af2513ef612e997b775d15f44b4a5770fa6482d9
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-11-27 21:09:18 -0500

    setup.py

---
+++ pyctr/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name="pyCTR",
+                 version="0.0.1",
+                 author="Aaron Mascaro",
+                 author_email="mascaroa1@gmail.com",
+                 url="",
+                 packages=setuptools.find_packages(),
+                 install_requires=['numpy',
+                                   'pandas>=0.25', ]
+                 )
