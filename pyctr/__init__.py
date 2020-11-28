beb2c12cea87a0dc309b73355104cb8c5f74bc67
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-11-27 21:08:46 -0500

    logger:

---
+++ pyctr/__init__.py
@@ -0,0 +1,5 @@
+import sys
+import logging
+
+log_fmt = f'%(asctime)s.%(msecs)03d - %(levelname)8s: %(funcName)10s -- %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_fmt)
