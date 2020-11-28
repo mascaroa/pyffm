49e067477d64e6454b5aafa12a1ee2c3a4c87a28
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-11-27 22:21:42 -0500

    template main class, setup basics

--- pyctr/pyctr.py
+++ pyctr/pyctr.py
@@ -1,11 +1,19 @@
 from typing import Union
 import pandas as pd
 
-from pyctr.engine.ctr_engine import CTREngine
+from engine.ffm_engine import FFMEngine
+from engine.fm_engine import FMEngine
+from engine.poly2_engine import Poly2Engine
+
+from util import exception_func
 
 import logging
 
 logger = logging.getLogger(__name__)
+
+ENGINE_DICT = {'ffm': FFMEngine,
+               'fm': FMEngine,
+               'poly2': Poly2Engine}
 
 
 class PyCTR:
@@ -21,11 +29,13 @@ class PyCTR:
         self.predict_from_file = io_params.get('train_from_file', False)
         model = 'FFM' if model is None else model
 
-        self.engine = CTREngine(model, training_params=training_params, io_params=io_params)
+        name_error_string = f'Model {model.lower()} not found! Must be in {ENGINE_DICT.keys()}'
+        self.engine = ENGINE_DICT.get(model.lower(), exception_func)(training_params=training_params, io_params=io_params)
 
     def train(self, data_in: Union[str, list, pd.DataFrame]):
         self._check_inputs(data_in)
-        # Format data and train here
+        formatted_data = self._format_train_data(data_in)
+        self.engine.train(formatted_data)
 
     def predict(self, x: Union[str, list, pd.DataFrame]):
         self._check_inputs(x)
@@ -39,8 +49,43 @@ class PyCTR:
             self.predict_from_file = True
         elif isinstance(x, pd.DataFrame):
             logger.info('DataFrame input detected')
-        elif isinstance(x, str):
+        elif isinstance(x, list):
             logger.info('List data detected')
 
-    def _format_train_data(self):
+    def _format_train_data(self, data_in: Union[str, list, pd.DataFrame]) -> list:
+        if isinstance(data_in, str):
+            logger.info('Loading file data')
+            return self._format_file_data(data_in)
+        elif isinstance(data_in, pd.DataFrame):
+            logger.info('Formatting dataframe')
+            return self._format_dataframe(data_in)
+        elif isinstance(data_in, list):
+            logger.info('Formatting list data')
+            return self._format_list_data(data_in)
+
+    def _format_dataframe(self, df_in: pd.DataFrame) -> list:
         pass
+
+    def _format_list_data(self, list_in: list) -> list:
+        pass
+
+    def _format_file_data(self, filename: str) -> list:
+        # TODO: Map features and fields!
+        data_in = []
+        with open('small_train.txt', 'r') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                click = [int(line.replace('\n', '').split(' ')[0])]
+                features = [(int(val.split(':')[0]), int(val.split(':')[1]), float(val.split(':')[2])) for val in line.replace('\n', '').split(' ')[1:]]
+                data_in.append(click + features)
+        return data_in
+
+
+if __name__ == '__main__':
+    pyctr = PyCTR(model='ffm')
+    import os
+
+    dir = os.getcwd()
+    pyctr.train(os.path.join(dir, 'test/integration_test/small_train.txt'))
