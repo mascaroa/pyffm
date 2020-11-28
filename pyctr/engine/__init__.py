6052a42278d014a7d908f67a2281526d03efde9c
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-11-27 23:44:43 -0500

    fix default:

--- pyctr/engine/__init__.py
+++ pyctr/engine/__init__.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class BaseEngine(ABC):
     def __init__(self, training_params, io_params):
         self.model = None
-        self.epochs = training_params.get('epochs', 100)
+        self.epochs = training_params.get('epochs', 10)
         self.learn_rate = training_params.get('learn_rate', 0.2)
 
     @abstractmethod
