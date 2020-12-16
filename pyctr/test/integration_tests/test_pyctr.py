2753c6e1c74b6857f8df8f26fd32e8e7f19b20cc
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-12-16 18:55:52 -0500

    working file train test

--- pyctr/test/integration_tests/test_pyctr.py
+++ pyctr/test/integration_tests/test_pyctr.py
@@ -8,27 +8,28 @@ from pyctr import PyCTR
 
 class TestCTREngine(unittest.TestCase):
     def setUp(self) -> None:
-        self.pyctr = PyCTR(model="ffm")
+        training_params = {'epoch': 10, 'reg_lambda': 0.002, 'sigmoid': True, 'parallel': False, 'early_stop': False}
+        self.pyctr = PyCTR(model="ffm", training_params=training_params)
 
     def test_train_from_file(self):
-
         import os
 
         dir = os.getcwd()
-        self.pyctr.train(os.path.join(dir, 'small_train.txt'))
-        preds = self.pyctr.predict(os.path.join(dir, 'small_train.txt'))
-        print(preds)
-        # assert some stuff here, write full tests eventually
+        self.pyctr.train(os.path.join(dir, 'bigdata.tr.txt'))
+        preds = self.pyctr.predict(os.path.join(dir, 'bigdata.te.txt'))
+        test_preds = np.array([0.19198589, 0.36800491, 0.26981428, 0.41630196, 0.2679637,
+                               0.33786355, 0.12455032, 0.21487805, 0.28094977, 0.16309123])
+        self.assertTrue(np.allclose(test_preds, preds[:10]))
 
     def test_train_from_datafraome(self):
+        training_params = {'epoch': 10, 'reg_lambda': 0.002, 'sigmoid': True, 'parallel': True}
+        self.pyctr = PyCTR(model="ffm", training_params=training_params)
+
         file_path = os.path.join(os.getcwd(), 'sample_df_train.csv')
         df_in = pd.read_csv(file_path, index_col=0)
-        # file_path = 'D:\\train_data.csv'
-        # for chunk in pd.read_csv(file_path, delimiter='|', index_col=0, iterator=True, chunksize=1_000_000):
-        #     df_in = chunk
-        #     break
-        df_in.reset_index(inplace=True)
         df_in.rename(columns={'label': 'click'}, inplace=True)
+
         self.pyctr.train(df_in)
-        self.pyctr.predict(df_in)
-        print(df_in)
+        preds = self.pyctr.predict(df_in)
+
+        print(preds)
