0a6b8252dc9073bb0fbacc9e3ed24c1b0e48b2b0
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-12-16 18:55:59 -0500

    reqs

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(name="pyCTR",
                  author_email="mascaroa1@gmail.com",
                  url="https://github.com/mascaroa/pyctr",
                  packages=setuptools.find_packages(),
-                 install_requires=['numpy',
-                                   'pandas>=0.25', ]
+                 install_requires=['numpy>=1.16',
+                                   'pandas>=0.24',
+                                   'numba>=0.49.1']
                  )
