836dbbfe7cc620707acdb0d91d767b59ed629d81
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-12-16 19:49:06 -0500

    purge prev. named files

--- pyctr/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pyctr import PyCTR
-
-import sys
-import logging
-
-# TODO: propagate logging... tricky
-log_fmt = f'[%(asctime)s.%(msecs)03d] - %(levelname)8s: %(name)10s -- %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_fmt)
