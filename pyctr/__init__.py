95c572a91c2f244b8f14ff8fd98acc585d0459fc
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2020-12-16 19:25:42 -0500

    logger format

--- pyctr/__init__.py
+++ pyctr/__init__.py
@@ -3,5 +3,6 @@ from .pyctr import PyCTR
 import sys
 import logging
 
-log_fmt = f'%(asctime)s.%(msecs)03d - %(levelname)8s: %(name)10s -- %(message)s'
+# TODO: propagate logging... tricky
+log_fmt = f'[%(asctime)s.%(msecs)03d] - %(levelname)8s: %(name)10s -- %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_fmt)
