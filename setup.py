27208f5b5c83a746db7dff21bb392a950db8ca7e
A Mascaro <aaron.mascaro@mail.mcgill.ca>
2021-01-17 14:42:38 -0500

    0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(name="pyFFM",
-                 version="0.0.3",
+                 version="0.0.4",
                  author="Aaron Mascaro",
                  author_email="mascaroa1@gmail.com",
                  description="Python implementation of Factorization Machines (+ Field Aware)",
